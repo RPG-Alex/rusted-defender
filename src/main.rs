c79215619d9ab68380fbc411b89dcbc6aa89ab81
RPG-Alex <45256796+RPG-Alex@users.noreply.github.com>
2023-09-10 09:31:04 +0800

    Sprite movement improved. Added placeholder background

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,24 @@ enum Direction {
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
+
+    // Adding the background
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("backgrounds/rusted-defender-home-screen.png"),
+        transform: Transform::from_xyz(0., 0., -10.), // Ensure Z-coordinate is behind other entities
+        ..default()
+    });
+
     commands.spawn((
         SpriteBundle {
             texture: asset_server.load("sprites/mech-sprite.png"),
             transform: Transform::from_xyz(0., 0., 0.),
             ..default()
         },
-        Direction::Up,
+        Direction::Left,
     ));
 }
+
 
 /// The sprite is animated by changing its translation depending on the time that has passed since
 /// the last frame.
@@ -37,20 +46,24 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
         match *logo {
             Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
             Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
-            Direction::Left => transform.translation.x += 150. * time.delta_seconds(),
-            Direction::Right => transform.translation.x -= 150. * time.delta_seconds(),
+            Direction::Right => transform.translation.x += 150. * time.delta_seconds(),
+            Direction::Left => transform.translation.x -= 150. * time.delta_seconds(),
         }
 
-        if transform.translation.y > 200. {
-            *logo = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *logo = Direction::Up;
+        // Check the boundaries and update the direction
+        match *logo {
+            Direction::Up if transform.translation.y >= 200. => *logo = Direction::Right,
+            Direction::Right if transform.translation.x >= 200. => *logo = Direction::Down,
+            Direction::Down if transform.translation.y <= -200. => *logo = Direction::Left,
+            Direction::Left if transform.translation.x <= -200. => *logo = Direction::Up,
+            _ => {}
         }
-        //not functional yet
-        if transform.translation.x > 200. {
-            *logo = Direction::Left;
-        } else if transform.translation.x < -200. {
-            *logo = Direction::Right;
-        }
+        
+        //println!("{:?}", transform);
+
+        
+
     }
+    
 }
+
