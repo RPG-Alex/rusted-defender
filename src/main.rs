92b0e2605e0728901784614df9eca9e544a1e163
RPG-Alex <mralexblood@outlook.com>
2023-11-14 19:33:42 +0800

    started updating new splash screen

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        //.add_systems(Startup)
-        //.add_systems(Update)
+        .add_systems(Startup, setup)
+        .add_systems(Update, {bevy::window::close_on_esc})
         .run();
 }
 
@@ -13,3 +13,17 @@ struct Player;
 
 #[derive(Component)]
 struct Enemy;
+
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((SpriteBundle {
+        sprite: Sprite { 
+            ..default()
+         },
+         texture: asset_server.load("backgrounds/splash.png"),
+         transform: Transform::from_xyz(0.0, 0.0, -10.0),
+         ..default()
+    }));
+}
