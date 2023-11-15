43ca75a86a7ab5b95c03ce8208778332d34d2164
Alex Blood <mralexblood@outlook.com>
2023-11-15 14:54:17 +0800

    Taken the UI example for a button from the Bevy Book

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use bevy::prelude::*;
+// Bevy recommended for better clippy use
+#![allow(clippy::type_complexity)]
+
+use bevy::{prelude::*, winit::WinitSettings};
+
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(WinitSettings::desktop_app())
         .add_systems(Startup, setup)
-        .add_systems(Update, {bevy::window::close_on_esc})
+        .add_systems(Update, (bevy::window::close_on_esc, button_system))
         .run();
 }
 
@@ -13,6 +18,44 @@ struct Player;
 
 #[derive(Component)]
 struct Enemy;
+
+// Taken from the bevy example
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+fn button_system(
+    mut interaction_query: Query<
+        (
+        &Interaction,
+        &mut BackgroundColor,
+        &mut BorderColor,
+        &Children,
+        ),
+        (Changed<Interaction>, With<Button>),
+        >,
+    mut text_query: Query<&mut Text>,
+) {
+    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Pressed => {
+                text.sections[0].value = "Lets Go".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = Color::RED;
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "Start the Adventure".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                text.sections[0].value = "START".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
+}
 
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -26,4 +69,45 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
          transform: Transform::from_xyz(0.0, 0.0, -10.0),
          ..default()
     }));
+
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(250.0),
+                        height: Val::Px(130.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    border_color: BorderColor(Color::BLACK),
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Button",
+                        TextStyle {
+                            //font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                            ..default()
+                        },
+                    ));
+                });
+        });
 }
